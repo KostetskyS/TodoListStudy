00fbb67361c0654f2867177e7175b34e7eae1b5a
KostetskyS <kostetsky1337@gmail.com>
2023-05-21 12:15:15 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import TodoList from "./todo/TodoList";
+import React, { useState, useEffect } from 'react';
+import Context from "./todo/context";
+import Loader from "./Loader";
+import Modal from "./modal/Modal";
 function App() {
+
+  const [todos, setTodos] = useState([]) ;
+  const [loading, setLoading] = useState(true)
+  const AddTodo = React.lazy(() => import('./todo/AddTodo'))
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        .then(response => response.json())
+        .then(todos => {
+          setTodos(todos)
+          setLoading(false)
+        })
+  }, [])
+
+  function toggleTodo (id) {
+    setTodos( todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo
+    }))
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(
+        todos.concat([{
+        title,
+        id: Date.now(),
+        completed: false
+      }
+    ]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>My Notes</h1>
+        <Modal/>
+        <React.Suspense fallback={<p>loading..</p>}> 
+          <AddTodo onCreate={addTodo}/> 
+        </React.Suspense>
+        {loading && <Loader/>}
+        {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : loading ? null : (
+          <p>list of notes is empty</p>
+        )}
+      </div>
+    </Context.Provider>
   );
 }
 
